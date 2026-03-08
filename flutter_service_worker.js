89342938aaf4cf1ed0e3c302c6d34fab372070c8
manualattendancedemo <manualattendance.demo@gmail.com>
2026-03-08 22:33:41 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ddea29f3c261be9c5d845240d5fbb20a
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8b50df90b21f6f99abc758a7560d2adc",
+"flutter_bootstrap.js": "9cb59f810de444fb5aafa93334e90e62",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
